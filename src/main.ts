f20ad6a9d73db668d3c3f8626b5eda49d4261f20
Andy <andersonvieira818@yahoo.com.br>
2024-03-29 23:15:00 -0300

    Remove items do main

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,3 @@ let customer = new Customer("123", "Anderson");
 const address = new Address("Rua 1", 1, "123123", "Natal");
 customer.Address = address
 customer.activate()
-
-const item1 = new OrderItem("1", "item1", 10)
-const item2 = new OrderItem("3", "item3", 20)
-const order = new Order("1", "123", [item1, item2])
